5e989c52ee5f57a63866af53b963f27884d1c93d
unknown <st10434060@vcconnect.edu.za>
2025-08-11 22:36:37 +0200

    Initial empty project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CloudRetailApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
